ddd777973c018b4d864af6ef234bc0a9fe889304
Stanislav <31danes@gmail.com>
2024-04-02 00:19:28 +0700

    Создание проекта

---
+++ src/main/java/ru/tomsk/App.java
@@ -0,0 +1,13 @@
+package ru.tomsk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "EnergoServer started" );
+    }
+}
