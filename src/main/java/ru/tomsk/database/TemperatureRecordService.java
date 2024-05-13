64b13a08312913b92f537195d9269c0038ec3cef
z1dns <47348740+z1dns@users.noreply.github.com>
2024-05-13 23:19:12 +0700

    Получение температурных записей из базы данных

--- src/main/java/ru/tomsk/database/TemperatureRecordService.java
+++ src/main/java/ru/tomsk/database/TemperatureRecordService.java
@@ -4,10 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.tomsk.temperature.TemperatureRecord;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +19,9 @@ public class TemperatureRecordService {
 
     public void addAll(List<TemperatureRecord> temperatureRecords) {
         try (Connection connection = connectionProvider.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO TemperatureRecords (trmId, time_stamp, surfaceTemperature, airTemperature) VALUES (?, ?, ?, ?)")) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO TemperatureRecords (trmId, time_stamp, surfaceTemperature, airTemperature) VALUES (?, ?, ?, ?)"
+            )) {
             for (var record : temperatureRecords) {
                 preparedStatement.setInt(1, record.deviceID());
                 preparedStatement.setTimestamp(2, Timestamp.from(record.timestamp()));
@@ -35,5 +35,31 @@ public class TemperatureRecordService {
                 LOGGER.warn("Insert records to DB error: {}", t.getMessage());
             }
         }
+    }
+
+    public List<TemperatureRecord> getAll() {
+        List<TemperatureRecord> temperatureRecords = new ArrayList<>();
+        try (Connection conn = this.connectionProvider.getConnection();
+             PreparedStatement preparedStatement = conn.prepareStatement(
+                     "SELECT trmId,time_stamp,surfaceTemperature,airTemperature FROM TemperatureRecords"
+             )) {
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                Instant timeStamp = rs.getTimestamp(2).toInstant();
+                float surfaceTemperature = rs.getFloat(3);
+                float airTemperature = rs.getFloat(4);
+                try {
+                    temperatureRecords.add(new TemperatureRecord(id, timeStamp, surfaceTemperature, airTemperature));
+                } catch (IllegalArgumentException e) {
+                    LOGGER.warn("Bad TemperatureRecord into DB: {}", e.getMessage());
+                }
+            }
+        } catch (SQLException e) {
+            for (Throwable t : e) {
+                LOGGER.warn("Get records from DB error: {}", t.getMessage());
+            }
+        }
+        return temperatureRecords;
     }
 }
