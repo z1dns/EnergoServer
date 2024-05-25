aa864fe8d8051049c552f4a194e38c8e35d74dc6
z1dns <47348740+z1dns@users.noreply.github.com>
2024-05-25 14:14:25 +0700

    DeviceIDTest - ассерты фреймворка jupiter

--- src/test/java/ru/tomsk/temperature/DeviceIDTest.java
+++ src/test/java/ru/tomsk/temperature/DeviceIDTest.java
@@ -1,54 +1,54 @@
 package ru.tomsk.temperature;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeviceIDTest {
     @Test
     public void testMinValue() {
         DeviceID deviceID = new DeviceID(100_001);
-        Assertions.assertEquals(1, deviceID.getTrmID());
-        Assertions.assertEquals(1, deviceID.getUspdID());
+        assertEquals(1, deviceID.getTrmID());
+        assertEquals(1, deviceID.getUspdID());
     }
     @Test
     public void testMinTrmUspd() {
         DeviceID deviceID = new DeviceID(1, 1);
-        Assertions.assertEquals(100_001, deviceID.getValue());
-        Assertions.assertEquals(1, deviceID.getTrmID());
-        Assertions.assertEquals(1, deviceID.getUspdID());
+        assertEquals(100_001, deviceID.getValue());
+        assertEquals(1, deviceID.getTrmID());
+        assertEquals(1, deviceID.getUspdID());
     }
 
     @Test
     public void testMaxValue() {
         DeviceID deviceID = new DeviceID(555_599_999);
-        Assertions.assertEquals(5555, deviceID.getTrmID());
-        Assertions.assertEquals(99_999, deviceID.getUspdID());
+        assertEquals(5555, deviceID.getTrmID());
+        assertEquals(99_999, deviceID.getUspdID());
     }
 
     @Test
     public void testMaxTrmUspd() {
         DeviceID deviceID = new DeviceID(7777, 99_999);
-        Assertions.assertEquals(777_799_999, deviceID.getValue());
-        Assertions.assertEquals(7777, deviceID.getTrmID());
-        Assertions.assertEquals(99_999, deviceID.getUspdID());
+        assertEquals(777_799_999, deviceID.getValue());
+        assertEquals(7777, deviceID.getTrmID());
+        assertEquals(99_999, deviceID.getUspdID());
     }
 
     @Test
     public void testValue() {
         DeviceID deviceID = new DeviceID(12_304_567);
-        Assertions.assertEquals(123, deviceID.getTrmID());
-        Assertions.assertEquals(4567, deviceID.getUspdID());
+        assertEquals(123, deviceID.getTrmID());
+        assertEquals(4567, deviceID.getUspdID());
     }
 
     @Test
     public void testTrmUspd() {
         DeviceID deviceID = new DeviceID(123, 4567);
-        Assertions.assertEquals(12_304_567, deviceID.getValue());
-        Assertions.assertEquals(123, deviceID.getTrmID());
-        Assertions.assertEquals(4567, deviceID.getUspdID());
+        assertEquals(12_304_567, deviceID.getValue());
+        assertEquals(123, deviceID.getTrmID());
+        assertEquals(4567, deviceID.getUspdID());
     }
 
     @Test
